691b58713c41d3c43fc653333f4e748514aa4881
cc0a <jmckeeco84@gmail.com>
2020-02-11 16:10:06 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/css/bootstrap.min.css';
+import './assets/css/agency.min.css';
+import './assets/scss/masthead.scss';
+import './assets/scss/navbar.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
