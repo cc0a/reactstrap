4a59f1f0f7cd1c97538924220764307bc2680553
cc0a <jmckeeco84@gmail.com>
2020-02-13 12:41:18 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   </header>
 
 <div class="louns-way">
-          <h3>The Lounsberry Way</h3> 
+          <h3 className="louns-way-heading">The Lounsberry Way</h3> 
           <p className="louns-way-content">For the last 25 years, Lounsberry Law has endeavored to deliver exceptional, affordable legal services to our clients. We believe there's something to be said for doing things right the first time, every time.</p>
 </div>
 
