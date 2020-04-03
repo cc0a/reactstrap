aadc92511a8ef3ec854c3c1e350e08d5ce17562b
cc0a <jmckeeco84@gmail.com>
2020-04-03 14:47:25 -0600

    updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import '../../assets/scss/navbar.scss';
 import { FaPhone, FaBars } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
-export class Navbar extends Component {
+export default class Navbar extends Component {
+    state = {
+      isOpen: false
+    };
+    handleToggle = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    };
     render() {
-        return (
-            <div className="nav">
-                <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="main-nav">
-                    <div class="container">
-                        <a class="navbar-brand js-scroll-trigger" id="nav-text" href="#page-top">Lounsberry Law</a>
-                        <a href="tel:970-660-4436"><FaPhone className="phone-icon"/><FaBars className="menu-icon"/></a>
-                        <h3 className="phone-number">970-660-4436</h3>
-                    </div>
-                </nav>
-            </div> 
-        )
+      return (
+        <nav className="navbar">
+          <div className="nav-center">
+            <div className="nav-header">
+              <Link to="/">
+                <img />
+              </Link>
+              <button
+                type="button"
+                className="nav-btn"
+                onClick={this.handleToggle}
+              >
+                <FaBars className="nav-icon" />
+              </button>
+            </div>
+            <ul
+              className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+            >
+              <li>
+                <Link to="/" id="nav-hide">Home</Link>
+              </li>
+              <li>
+                <Link to="/rooms" id="nav-hide">Rooms</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
     }
-}
-
-export default Navbar
+  }
