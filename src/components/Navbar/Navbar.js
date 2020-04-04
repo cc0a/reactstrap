94942552f5fcc38f478d6bb8e779725e8cd1c807
cc0a <jmckeeco84@gmail.com>
2020-04-03 21:35:58 -0600

    updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,7 +25,7 @@ export class Navbar extends Component {
                     </div>      
 
                     <div className="btn" onClick={this.handleClick}>
-                        <a href="tel:970-660-4436"><FaPhone className="nav-btn" id="phone"/></a>
+                        <a href="tel:970-660-4436"><FaPhone className="nav-btn" id="phone-icon"/></a>
                         <FaBars className="nav-btn"/>
                     </div> 
                 </div>
